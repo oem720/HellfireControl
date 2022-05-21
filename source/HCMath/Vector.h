1cb4ceec69773cfa769f49507aa012adb7b14e62
Owen Meyers <OEMeyers@student.fullsail.edu>
2022-05-21 19:14:34 -0400

    Completed layout of the operators and functions of Vec2. This includes full swizzle support (including naming for vectors treated like RGBA) and fully overloaded operators. Provisionally, the Cross Product has been placed on the & operator, while the Dot Product has been placed on the * operator. Full assignment operations from those operators is planned.

--- source/HCMath/Vector.h
+++ source/HCMath/Vector.h
@@ -15,6 +15,28 @@ public:
 	inline Vec2();
 	inline Vec2(float _x, float _y);
 	inline Vec2(const Vec2& _other);
+	inline Vec2& operator=(const Vec2& _other);
+	inline Vec2& operator+(const Vec2& _other);
+	inline Vec2& operator+=(const Vec2& _other);
+	inline Vec2& operator-(const Vec2& _other);
+	inline Vec2& operator-=(const Vec2& _other);
+	inline Vec2& operator*(const float _scale);
+	inline Vec2& operator*=(const float _scale);
+	inline float operator*(const Vec2& _other);
+	inline float operator*=(const Vec2& _other);
+	inline float operator&(const Vec2& _other);
+	inline float operator&=(const Vec2& _other);
+	inline float Dot(const Vec2& _other);
+	inline float Cross(const Vec2& _other);
+	inline Vec2 Add(const Vec2& _other);
+	inline Vec2 Subtract(const Vec2& _other);
+	inline Vec2 Scale(const Vec2& _other);
+	inline float& X();
+	inline float& Y();
+	inline Vec2 XX() const;
+	inline Vec2 YY() const;
+	inline Vec2 RR() const;
+	inline Vec2 GG() const;
 };
 
 class alignas(16) Vec3 {

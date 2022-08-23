3b3cad0e4e421309561f8c6a75dabd3b03a43d7d
Owen Meyers <owenmeyers.sc@gmail.com>
2022-08-22 23:04:43 -0400

    Began work on the matrix types require for this math library. All of them are 4x4 matrices, each in a different underlying type. MatrixF is in floats, MatrixD is in doubles and MatrixI is in ints.

---
+++ source/HCMath/HCMatrix.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "HCVector.h"
+
+struct MatrixF {
+	Vec4F m_vRows[4];
+
+
+};
+
+struct MatrixD {
+	Vec4D m_vRows[4];
+
+
+};
+
+struct MatrixI {
+	Vec4I m_vRows[4];
+
+
+};
--- source/Main.cpp
+++ source/Main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include "HCMath/HCVector.h"
+#include "HCMath/HCMatrix.h"
 
 int __cdecl main() {
 	std::cout << "Hellfire Control Engine\n----------------------------------------------" << std::endl;

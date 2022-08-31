1333151890e89f7d6bf77e4cab16f3f73e556f25
Owen Meyers <owenmeyers.sc@gmail.com>
2022-08-30 21:27:26 -0400

    Began work on the matrix library and added an All-In-One header file for including the math library.

--- source/HCMath/HCMatrix.h
+++ source/HCMath/HCMatrix.h
@@ -5,6 +5,9 @@
 struct MatrixF {
 	Vec4F m_vRows[4];
 
+	HC_INLINE MatrixF() {};
+	HC_INLINE MatrixF(Vec4F _vRow1, Vec4F _vRow2, Vec4F _vRow3, Vec4F _vRow4) { m_vRows[0] = _vRow1; m_vRows[1] = _vRow2; m_vRows[2] = _vRow3; m_vRows[3] = _vRow4; }
+	HC_INLINE MatrixF(Vec4F* _vRows) { m_vRows[0] = _vRows[0]; m_vRows[1] = _vRows[1]; m_vRows[2] = _vRows[2]; m_vRows[3] = _vRows[3]; }
 
 };
 
@@ -19,3 +22,4 @@ struct MatrixI {
 
 
 };
+

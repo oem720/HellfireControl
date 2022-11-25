55c3c857e93cd0253fab2179a9f0feabca33cc21
Owen Meyers <owenmeyers.sc@gmail.com>
2022-11-24 23:47:59 -0500

    Reorganized the repository to fix the poorly put together CMAKE scripts. Added placeholder files to maintain current directory structure

--- source/Main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include "HCMath/HCMatrix.h"
-
-int __cdecl main() {
-	std::cout << "Hellfire Control Engine\n----------------------------------------------" << std::endl;
-
-	return 0;
-}

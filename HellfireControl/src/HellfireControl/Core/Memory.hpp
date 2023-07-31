85ae6e523900e4e1f69fa84fe7f7426afd81e1f8
Owen Meyers <owenmeyers.sc@gmail.com>
2023-07-30 23:20:04 -0400

    Basic file creation for all allocator types and for the memory subsystem

---
+++ HellfireControl/src/HellfireControl/Core/Memory.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <HellfireControl/Core/Memory/BlockAllocator.hpp>
+#include <HellfireControl/Core/Memory/StackAllocator.hpp>
+#include <HellfireControl/Core/Memory/PoolAllocator.hpp>
+#include <HellfireControl/Core/Memory/HeapAllocator.hpp>
+
+class MemorySubsystem {
+
+};

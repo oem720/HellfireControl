0d3708b41938e1f22335c1c951760be1dce3f5ae
Owen Meyers <owenmeyers.sc@gmail.com>
2023-10-05 10:09:37 -0400

    Setup work for memory manager.

--- HellfireControl/src/HellfireControl/Core/Memory.hpp
+++ HellfireControl/src/HellfireControl/Core/Memory.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
-#include <HellfireControl/Core/Memory/BlockAllocator.hpp>
-#include <HellfireControl/Core/Memory/StackAllocator.hpp>
-#include <HellfireControl/Core/Memory/PoolAllocator.hpp>
-#include <HellfireControl/Core/Memory/HeapAllocator.hpp>
+void* operator new (size_t _sSize);
+
+void* operator new[](size_t _sSize);
+
+void operator delete(void* _ptrDelete);
+
+void operator delete[](void* _ptrDeleteArr);
 
 class MemorySubsystem {
+private:
+	friend void* operator new(size_t _sSize);
 
+	friend void* operator new[](size_t _sSize);
+
+	friend void operator delete(void* _ptrDelete);
+
+	friend void operator delete[](void* _ptrDeleteArr);
+
+public:
+	MemorySubsystem() {}
+
+	~MemorySubsystem() {}
+
+	void Init();
+
+	void Shutdown();
 };

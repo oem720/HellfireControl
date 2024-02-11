e9262a8a8c52f18cfde0cc0c3fc3a3e8f203f1e1
Robert Sale <robertmsale@icloud.com>
2024-02-11 10:25:32 -0800

    Added AppDelegate, changed TorchLight to conditionally build MacOSX_BNDL
    Basically the app has to be a MacOSX Bundle for MacOS to allow window creation. Also windows need an AppDelegate to handle lifecycle events

--- HellfireControl/src/Platform/Mac/MacWindow.mm
+++ HellfireControl/src/Platform/Mac/MacWindow.mm
@@ -1,6 +1,19 @@
 #include <AppKit/AppKit.h>
 #include <Platform/Mac/MacWindow.hpp>
 
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+@public NSWindow* window;
+}
+@end
+@implementation AppDelegate
+- (void)applicationDidFinishLaunching:(NSNotification *) aNotification {
+    [window makeKeyAndOrderFront:nil];
+}
+- (void)setWindowPtr:(NSWindow *) to {
+    window = to;
+}
+@end
+
 namespace {
     constexpr NSWindowStyleMask styleMask[] = {
             NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable,
@@ -15,9 +28,12 @@ namespace {
         if ((isThis & styleMask[2]) == styleMask[2]) return 2;
         return 3;
     }
+    static AppDelegate* appDelegate;
+
 }
 
 void PlatformWindow::InitWindow(uint64_t& _u64Handle, uint8_t _u8Type, const std::string& _strName, const Vec2F& _v2Size, const Vec2F& _v2Loc) {
+    appDelegate = [[AppDelegate alloc] init];
     NSString *name = [NSString stringWithUTF8String:_strName.c_str()];
     NSRect frame = NSMakeRect(_v2Loc.x, _v2Loc.y, _v2Size.x, _v2Size.y);
 
@@ -27,6 +43,12 @@ void PlatformWindow::InitWindow(uint64_t& _u64Handle, uint8_t _u8Type, const std
         defer:NO];
     [window setTitle:name];
     _u64Handle = reinterpret_cast<uint64_t>(window);
+
+    [appDelegate setWindowPtr:window];
+
+    NSApplication *app = [NSApplication sharedApplication];
+    [app setDelegate:appDelegate];
+    [app run];
 }
 
 [[nodiscard]] bool PlatformWindow::SetWindowName(uint64_t _u64Handle, const std::string &_strName) {
@@ -39,7 +61,7 @@ void PlatformWindow::InitWindow(uint64_t& _u64Handle, uint8_t _u8Type, const std
 [[nodiscard]] Vec2F PlatformWindow::GetWindowSize(uint64_t _u64Handle) {
     auto *window = reinterpret_cast<NSWindow *>(_u64Handle);
     NSRect frame = [window frame];
-    return Vec2F(frame.size.width, frame.size.height);
+    return Vec2F{frame.size.width, frame.size.height};
 }
 
 [[nodiscard]] bool PlatformWindow::SetWindowSize(uint64_t _u64Handle, const Vec2F &_v2Size) {

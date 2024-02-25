f9f1a2edf22e7f701374deaf388824fca52ed7d0
Cameron Taylor <35664800+FunkyMoneyMan@users.noreply.github.com>
2024-02-24 16:43:05 -0800

    Fixed some compile errors with the singleton class.

--- HellfireControl/src/HellfireControl/UI/UI.cpp
+++ HellfireControl/src/HellfireControl/UI/UI.cpp
@@ -1,3 +1,14 @@
 #include <HellfireControl/UI/UI.hpp>
 
-UI::UI() {}
+UISubsystem* UISubsystem::m_UIInstance = nullptr;
+
+void UISubsystem::Init() {
+	
+}
+
+UISubsystem* UISubsystem::GetInstance() {
+	if (!m_UIInstance) {
+		m_UIInstance = new UISubsystem();
+	}
+	return m_UIInstance;
+};

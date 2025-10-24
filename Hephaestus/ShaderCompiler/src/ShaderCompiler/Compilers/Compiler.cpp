c3391c86e6edf67fc51e45dd4e33b3a63a3cfae8
Owen Meyers <owenmeyers.sc@gmail.com>
2025-10-24 12:44:45 -0400

    All of the most commonly used shader vars are now supported in the reflection system. Final 4 + debug display info remain.

--- Hephaestus/ShaderCompiler/src/ShaderCompiler/Compilers/Compiler.cpp
+++ Hephaestus/ShaderCompiler/src/ShaderCompiler/Compilers/Compiler.cpp
@@ -15,14 +15,30 @@ uint32_t GetRuntimeArrayStride(const spirv_cross::Compiler& _cComp, const spirv_
 	return _cComp.type_struct_member_array_stride(stType, stType.member_types.size() - 1);;
 }
 
+uint16_t TranslateBaseTypeToFlag(const spirv_cross::SPIRType::BaseType _btType) {
+	switch (_btType) {
+	case spirv_cross::SPIRType::BaseType::Boolean: return IO_BOOL;
+	case spirv_cross::SPIRType::BaseType::SByte: return IO_SIGNED_BYTE;
+	case spirv_cross::SPIRType::BaseType::UByte: return IO_UNSIGNED_BYTE;
+	case spirv_cross::SPIRType::BaseType::Short: return IO_SIGNED_SHORT;
+	case spirv_cross::SPIRType::BaseType::UShort: return IO_UNSIGNED_SHORT;
+	case spirv_cross::SPIRType::BaseType::Int: return IO_SIGNED_INT;
+	case spirv_cross::SPIRType::BaseType::UInt: return IO_UNSIGNED_INT;
+	case spirv_cross::SPIRType::BaseType::Half: return IO_HALF_FLOAT;
+	case spirv_cross::SPIRType::BaseType::Float: return IO_FLOAT;
+	case spirv_cross::SPIRType::BaseType::Double: return IO_DOUBLE_FLOAT;
+	default: return IO_INVALID;
+	}
+}
+
 std::vector<HCShaderVar> ShaderCompiler::ReflectSPIRV(const std::vector<uint32_t>& _vCodeBlob) {
 	Console::DebugInfo("Gathering CPU-exposed shader variables...");
 
 	spirv_cross::Compiler cComp(const_cast<uint32_t*>(_vCodeBlob.data()), _vCodeBlob.size());
 
-	//std::unordered_set<spirv_cross::VariableID> active = cComp.get_active_interface_variables();
+	std::unordered_set<spirv_cross::VariableID> active = cComp.get_active_interface_variables();
 	spirv_cross::ShaderResources srRes = cComp.get_shader_resources();
-	//cComp.set_enabled_interface_variables(std::move(active));
+	cComp.set_enabled_interface_variables(std::move(active));
 
 	return ParseShaderVars(cComp, srRes);
 }
@@ -170,22 +186,38 @@ std::vector<HCShaderVar> ShaderCompiler::ParseShaderVars(const spirv_cross::Comp
 
 	//Samplers
 	{
-		for (const auto& aSampler : _srRes.sampled_images) {
-			vShaderVars.push_back({
-				.m_svtType = VAR_COMBINED_IMAGE_SAMPLER,
-				.m_strVarName = _cComp.get_name(aSampler.id),
-				.m_pParameters = std::make_shared<SamplerParameters>(
-
-				)
-			});
-		}
-
 		for (const auto& aSampler : _srRes.separate_samplers) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aSampler.type_id);
+
+			uint8_t u8Flags = !stType.array.empty();
+
 			vShaderVars.push_back({
 				.m_svtType = VAR_SAMPLER,
 				.m_strVarName = _cComp.get_name(aSampler.id),
 				.m_pParameters = std::make_shared<SamplerParameters>(
+					_cComp.get_decoration(aSampler.id, spv::DecorationBinding),
+					_cComp.get_decoration(aSampler.id, spv::DecorationDescriptorSet),
+					u8Flags,
+					stType.array.empty() ? 0 : stType.array[0]
+				)
+			});
+		}
 
+		for (const auto& aSampler : _srRes.sampled_images) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aSampler.type_id);
+
+			uint8_t u8Flags = !stType.array.empty();
+
+			vShaderVars.push_back({
+				.m_svtType = VAR_COMBINED_IMAGE_SAMPLER,
+				.m_strVarName = _cComp.get_name(aSampler.id),
+				.m_pParameters = std::make_shared<CombinedSamplerParameters>(
+					_cComp.get_decoration(aSampler.id, spv::DecorationBinding),
+					_cComp.get_decoration(aSampler.id, spv::DecorationDescriptorSet),
+					u8Flags,
+					stType.array.empty() ? 0 : stType.array[0],
+					stType.image.dim,
+					stType.image.format
 				)
 			});
 		}
@@ -194,25 +226,153 @@ std::vector<HCShaderVar> ShaderCompiler::ParseShaderVars(const spirv_cross::Comp
 	//Inputs
 	{
 		for (const auto& aInput : _srRes.stage_inputs) {
-			vShaderVars.push_back({
-				.m_svtType = VAR_STAGE_INPUTS,
-				.m_strVarName = _cComp.get_name(aInput.id),
-				.m_pParameters = std::make_shared<SamplerParameters>(
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aInput.type_id);
 
+			uint16_t u16Flags = !stType.array.empty() | TranslateBaseTypeToFlag(stType.basetype);
+
+			uint8_t u8InterpFlags = INTERP_SMOOTH;
+			u8InterpFlags |= _cComp.has_decoration(aInput.id, spv::DecorationFlat) ? INTERP_FLAT : 0;
+			u8InterpFlags |= _cComp.has_decoration(aInput.id, spv::DecorationNoPerspective) ? INTERP_NO_PERSPECTIVE : 0;
+			u8InterpFlags |= _cComp.has_decoration(aInput.id, spv::DecorationCentroid) ? INTERP_CENTROID : 0;
+			u8InterpFlags |= _cComp.has_decoration(aInput.id, spv::DecorationSample) ? INTERP_SAMPLE : 0;
+
+			vShaderVars.push_back({
+				.m_svtType = VAR_STAGE_INPUT,
+				.m_strVarName = _cComp.get_name(aInput.id),
+				.m_pParameters = std::make_shared<InputOutputParameters>(
+					_cComp.get_decoration(aInput.id, spv::DecorationLocation),
+					_cComp.get_decoration(aInput.id, spv::DecorationComponent),
+					u16Flags,
+					stType.vecsize,
+					stType.columns,
+					stType.array.empty() ? 0 : stType.array[0],
+					u8InterpFlags
+				)
+			});
+		}
+
+		for (const auto& aInput : _srRes.builtin_inputs) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aInput.resource.type_id);
+
+			vShaderVars.push_back({
+				.m_svtType = VAR_BUILTIN_STAGE_INPUT,
+				.m_strVarName = _cComp.get_name(aInput.resource.id),
+				.m_pParameters = std::make_shared<BuiltinIOParameters>(
+					aInput.builtin,
+					stType.storage,
+					TranslateBaseTypeToFlag(stType.basetype),
+					stType.vecsize,
+					stType.columns
 				)
 			});
 		}
 
 		for (const auto& aSubpassInput : _srRes.subpass_inputs) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aSubpassInput.type_id);
+
 			vShaderVars.push_back({
 				.m_svtType = VAR_SUBPASS_INPUT,
 				.m_strVarName = _cComp.get_name(aSubpassInput.id),
-				.m_pParameters = std::make_shared<SamplerParameters>(
-
+				.m_pParameters = std::make_shared<SubpassInputParameters>(
+					_cComp.get_decoration(aSubpassInput.id, spv::DecorationBinding),
+					_cComp.get_decoration(aSubpassInput.id, spv::DecorationDescriptorSet),
+					_cComp.get_decoration(aSubpassInput.id, spv::DecorationInputAttachmentIndex),
+					stType.image.dim,
+					stType.image.format
 				)
 			});
 		}
 	}
+
+	//Outputs
+	{
+		for (const auto& aOutput : _srRes.stage_outputs) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aOutput.type_id);
+
+			uint16_t u16Flags = !stType.array.empty() | TranslateBaseTypeToFlag(stType.basetype);
+
+			uint8_t u8InterpFlags = INTERP_SMOOTH;
+			u8InterpFlags |= _cComp.has_decoration(aOutput.id, spv::DecorationFlat) ? INTERP_FLAT : 0;
+			u8InterpFlags |= _cComp.has_decoration(aOutput.id, spv::DecorationNoPerspective) ? INTERP_NO_PERSPECTIVE : 0;
+			u8InterpFlags |= _cComp.has_decoration(aOutput.id, spv::DecorationCentroid) ? INTERP_CENTROID : 0;
+			u8InterpFlags |= _cComp.has_decoration(aOutput.id, spv::DecorationSample) ? INTERP_SAMPLE : 0;
+
+			vShaderVars.push_back({
+				.m_svtType = VAR_STAGE_OUTPUT,
+				.m_strVarName = _cComp.get_name(aOutput.id),
+				.m_pParameters = std::make_shared<InputOutputParameters>(
+					_cComp.get_decoration(aOutput.id, spv::DecorationLocation),
+					_cComp.get_decoration(aOutput.id, spv::DecorationComponent),
+					u16Flags,
+					stType.vecsize,
+					stType.columns,
+					stType.array.empty() ? 0 : stType.array[0],
+					u8InterpFlags
+				)
+			});
+		}
+
+		for (const auto& aOutput : _srRes.builtin_outputs) {
+			const spirv_cross::SPIRType& stType = _cComp.get_type(aOutput.resource.type_id);
+
+			vShaderVars.push_back({
+				.m_svtType = VAR_BUILTIN_STAGE_OUTPUT,
+				.m_strVarName = _cComp.get_name(aOutput.resource.id),
+				.m_pParameters = std::make_shared<BuiltinIOParameters>(
+					aOutput.builtin,
+					stType.storage,
+					TranslateBaseTypeToFlag(stType.basetype),
+					stType.vecsize,
+					stType.columns
+				)
+				});
+		}
+	}
+
+	//Misc
+	{
+		for (const auto& aAcceleration : _srRes.acceleration_structures) {
+			vShaderVars.push_back({
+				.m_svtType = VAR_ACCELERATION_STRUCTURE,
+				.m_strVarName = _cComp.get_name(aAcceleration.id),
+				.m_pParameters = std::make_shared<AccelerationStructureParameters>(
+
+				)
+			});
+		}
+
+		for (const auto& aShaderBuffer : _srRes.shader_record_buffers) {
+			vShaderVars.push_back({
+				.m_svtType = VAR_SHADER_RECORD_BUFFER,
+				.m_strVarName = _cComp.get_name(aShaderBuffer.id),
+				.m_pParameters = std::make_shared<ShaderRecordBufferParameters>(
+
+				)
+				});
+		}
+
+		for (const auto& aAtomicCounter : _srRes.atomic_counters) {
+			vShaderVars.push_back({
+				.m_svtType = VAR_ATOMIC_COUNTER,
+				.m_strVarName = _cComp.get_name(aAtomicCounter.id),
+				.m_pParameters = std::make_shared<AtomicCounterParameters>(
+
+				)
+				});
+		}
+
+		for (const auto& aGLBuffer : _srRes.gl_plain_uniforms) {
+			vShaderVars.push_back({
+				.m_svtType = VAR_GL_PLAIN_UNIFORM,
+				.m_strVarName = _cComp.get_name(aGLBuffer.id),
+				.m_pParameters = std::make_shared<GLPlainUniformBufferParameters>(
+
+				)
+				});
+		}
+	}
+
+	Console::DebugSuccess("Successfully gathered shader resources. Final count: " + std::to_string(vShaderVars.size()) + " entries");
 
 	return vShaderVars;
 }
--- Hephaestus/ShaderCompiler/src/ShaderCompiler/Compilers/ShaderVar.hpp
+++ Hephaestus/ShaderCompiler/src/ShaderCompiler/Compilers/ShaderVar.hpp
@@ -23,6 +23,28 @@ enum HCShaderVarType : uint8_t {
 	VAR_GL_PLAIN_UNIFORM
 };
 
+enum HCInputOutputType : uint16_t {
+	IO_INVALID = 0,
+	IO_BOOL = (1 << 1),
+	IO_SIGNED_BYTE = (1 << 2),
+	IO_UNSIGNED_BYTE = (1 << 3),
+	IO_SIGNED_SHORT = (1 << 4),
+	IO_UNSIGNED_SHORT = (1 << 5),
+	IO_SIGNED_INT = (1 << 6),
+	IO_UNSIGNED_INT = (1 << 7),
+	IO_HALF_FLOAT = (1 << 8),
+	IO_FLOAT = (1 << 9),
+	IO_DOUBLE_FLOAT = (1 << 10)
+};
+
+enum HCInterpolationType : uint8_t {
+	INTERP_SMOOTH = 0,
+	INTERP_FLAT = (1 << 0),
+	INTERP_NO_PERSPECTIVE = (1 << 1),
+	INTERP_CENTROID = (1 << 2),
+	INTERP_SAMPLE = (1 << 3)
+};
+
 struct ShaderVarParameters {};
 
 struct HCShaderVar {
@@ -87,15 +109,82 @@ struct ImageParameters : public ShaderVarParameters {
 };
 
 struct SamplerParameters : public ShaderVarParameters {
+	uint32_t m_u32Binding = 0;
+	uint32_t m_u32Set = 0;
+	uint8_t m_u8Flags = 0;
+	uint32_t m_u32ArraySize = 0;
 
+	SamplerParameters(uint32_t _u32Binding, uint32_t _u32Set, uint8_t _u8Flags, uint32_t _u32ArraySize)
+		: m_u32Binding(_u32Binding)
+		, m_u32Set(_u32Set)
+		, m_u8Flags(_u8Flags)
+		, m_u32ArraySize(_u32ArraySize) {}
+};
+
+struct CombinedSamplerParameters : public ShaderVarParameters {
+	uint32_t m_u32Binding = 0;
+	uint32_t m_u32Set = 0;
+	uint8_t m_u8Flags = 0;
+	uint32_t m_u32ArraySize = 0;
+	uint32_t m_u32Dimension = 0;
+	uint32_t m_u32Format = 0;
+
+	CombinedSamplerParameters(uint32_t _u32Binding, uint32_t _u32Set, uint8_t _u8Flags, uint32_t _u32ArraySize, uint32_t _u32Dimension, uint32_t _u32Format)
+		: m_u32Binding(_u32Binding)
+		, m_u32Set(_u32Set)
+		, m_u8Flags(_u8Flags)
+		, m_u32ArraySize(_u32ArraySize)
+		, m_u32Dimension(_u32Dimension) 
+		, m_u32Format(_u32Format) {}
 };
 
 struct InputOutputParameters : public ShaderVarParameters {
+	uint32_t m_u32Location = 0;
+	uint32_t m_u32Component = 0;
+	uint16_t m_u16Flags = 0;
+	uint32_t m_u32VecSize = 0;
+	uint32_t m_u32ColumnSize = 0;
+	uint32_t m_u32ArraySize = 0;
+	uint8_t m_u8InterpolationType = 0;	
 
+	InputOutputParameters(uint32_t _u32Location, uint32_t _u32Component, uint16_t _u16Flags, uint32_t _u32VecSize, uint32_t _u32ColumnSize, uint32_t _u32ArraySize, uint8_t _u8InterpolationType)
+		: m_u32Location(_u32Location)
+		, m_u32Component(_u32Component)
+		, m_u16Flags(_u16Flags)
+		, m_u32VecSize(_u32VecSize)
+		, m_u32ColumnSize(_u32ColumnSize)
+		, m_u32ArraySize(_u32ArraySize)
+		, m_u8InterpolationType(_u8InterpolationType) {}
+};
+
+struct BuiltinIOParameters : public ShaderVarParameters {
+	uint32_t m_u32BuiltinEnum = 0;
+	uint32_t m_u32StorageClass = 0;
+	uint16_t m_u16Type = 0;
+	uint32_t m_u32VecSize = 0;
+	uint32_t m_u32ColumnSize = 0;
+
+	BuiltinIOParameters(uint32_t _u32BuiltinEnum, uint32_t _u32StorageClass, uint16_t _u16Type, uint32_t _u32VecSize, uint32_t _u32ColumnSize)
+		: m_u32BuiltinEnum(_u32BuiltinEnum)
+		, m_u32StorageClass(_u32StorageClass)
+		, m_u16Type(_u16Type)
+		, m_u32VecSize(_u32VecSize)
+		, m_u32ColumnSize(_u32ColumnSize) {}
 };
 
 struct SubpassInputParameters : public ShaderVarParameters {
+	uint32_t m_u32Binding = 0;
+	uint32_t m_u32Set = 0;
+	uint32_t m_u32InputAttachmentIndex = 0;
+	uint32_t m_u32Dimension = 0;
+	uint32_t m_u32Format = 0;
 
+	SubpassInputParameters(uint32_t _u32Binding,uint32_t _u32Set, uint32_t _u32InputAttachmentIndex, uint32_t _u32Dimension, uint32_t _u32Format)
+		: m_u32Binding(_u32Binding)
+		, m_u32Set(_u32Set)
+		, m_u32InputAttachmentIndex(_u32InputAttachmentIndex)
+		, m_u32Dimension(_u32Dimension)
+		, m_u32Format(_u32Format) {}
 };
 
 struct AccelerationStructureParameters : public ShaderVarParameters {
